e38f2756a543aaadf1bcc95cdc502f540b48b7c6
Guilherme <guiteixeira1926@gmail.com>
2025-05-19 19:00:34 -0300

    feat: add graceful shutdown to the http server

--- cmd/fintrack/main.go
+++ cmd/fintrack/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Guizzs26/fintrack/internal/app"
 	"github.com/Guizzs26/fintrack/internal/config"
@@ -22,7 +29,25 @@ func main() {
 	router := app.NewRouter()
 	srv := app.NewServer(cfg.Server, router)
 
-	if err := srv.ListenAndServe(); err != nil {
-		log.Fatal(err)
+	go func() {
+		log.Printf("🚀 Server is running on %s", cfg.Server.Addr)
+		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("❌ HTTP server error: %v", err)
+		}
+		log.Println("Stopped serving new connections")
+
+	}()
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
+	<-sc
+	log.Println("Shutting down server...")
+
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownRelease()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("❌ HTTP shutdown error: %v", err)
 	}
+	log.Println("Graceful shutdown complete")
 }
